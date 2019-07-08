cb7ce84fca128a8cfc8f914e12a614a36be1fe14
Ygor <ygor.maniz@gmail.com>
2019-07-08 10:44:35 -0300

    Migrating to AndroidX

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,7 +1,7 @@
 package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import br.com.alura.financask.R
 import br.com.alura.financask.ui.adapter.ListaTransacoesAdapter
